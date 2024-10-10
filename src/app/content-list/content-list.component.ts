5cd4b0e045b043e9b9339ac16f63753b3cdd73d8
DARKSouL1607 <VP138@myscc.ca>
2024-10-10 18:08:39 -0400

    Added content list with laptops and basic styles

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-content-list',
   standalone: true,
-  imports: [],
+  imports: [], 
   templateUrl: './content-list.component.html',
-  styleUrl: './content-list.component.css'
+  styleUrls: ['./content-list.component.css' ] 
 })
 export class ContentListComponent {
-
+  
+  contentList = [
+    { id: 1, title: 'MSI', description: 'GAMING LAPTOP' },
+    { id: 2, title: 'DELL', description: 'PROFESSIONAL LAPTOP' },
+    { id: 3, title: 'ASUS', description: 'HIGH END LAPTO' },
+    { id: 4, title: 'MACBOOK', description: 'WORK SPACE LAPTOP' }
+  ];
 }
