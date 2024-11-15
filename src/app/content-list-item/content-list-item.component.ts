10044e8868f9192ec0140d4a0292b81aa9033f55
DARKSouL1607 <VP138@myscc.ca>
2024-11-14 19:17:53 -0500

    Fix property name to match input binding in ContentListItemComponent

--- src/app/content-list-item/content-list-item.component.ts
+++ src/app/content-list-item/content-list-item.component.ts
@@ -2,11 +2,10 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content-list-item',
-  standalone: true, // Add this line
+  standalone: true,
   templateUrl: './content-list-item.component.html',
   styleUrls: ['./content-list-item.component.css']
 })
 export class ContentListItemComponent {
-  @Input() contentItem: { id: number; title: string; description: string } | undefined; 
-} 
-
+  @Input() contentItem: { id: number; title: string; description: string } | undefined;
+}
